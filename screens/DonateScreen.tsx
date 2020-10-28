256475ebb0e9f431d72e056805ac792e0c67ddc4
madrigal1 <kurupgokul11@gmail.com>
2020-10-28 15:49:06 +0530

    feat: situation room

--- screens/DonateScreen.tsx
+++ screens/DonateScreen.tsx
@@ -1,36 +1,83 @@
 import * as React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 import EditScreenInfo from "../components/EditScreenInfo";
-import { Text, View } from "../components/Themed";
+
+import MapView from "react-native-maps";
+import ProfilePicture from "../components/ProfilePicture";
+import users from "../data/users";
 
 export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
+      <View style={styles.header}>
+        <View style={{ marginRight: 2 }}>
+          <Text style={styles.subtitle}>Current Disaster Location</Text>
+          <Text style={styles.title}>Chennai Floods</Text>
+        </View>
+        <ProfilePicture image={users[0].profilePic} size={50} />
+      </View>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 12.9716,
+          longitude: 77.5946,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
       />
-      <EditScreenInfo path="/screens/TabTwoScreen.js" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: "flex-end",
     alignItems: "center",
-    justifyContent: "center",
+  },
+  map: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    zIndex: 0,
+  },
+  header: {
+    position: "absolute",
+    top: 70,
+    left: 20,
+    backgroundColor: "white",
+    width: "90%",
+    height: 100,
+    zIndex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 10,
+    borderRadius: 8,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+  },
+  subtitle: {
+    color: "#838282",
+    fontSize: 14,
+    fontWeight: "300",
   },
   title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
+    color: "black",
+    fontSize: 36,
+    marginTop: 10,
   },
 });
